dad870e74a529211e98d2febe59c7db471563463
PyaePhyoPaing-mfu <6531503175@lamduan.mfu.ac.th>
2025-07-07 13:20:33 +0700

    debugging

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain_google_vertexai import VertexAI
 from langgraph.graph import StateGraph, END
 from google.oauth2.service_account import Credentials
 from google.auth import default
-from langchain_community.llms import HuggingFaceHub
+from langchain.llms import HuggingFaceHub
 import time
 import json
 
